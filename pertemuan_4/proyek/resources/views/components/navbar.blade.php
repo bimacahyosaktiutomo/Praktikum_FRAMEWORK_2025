4e642cf96e9bae47002462d85ae261ddebfbe30c
bimacahyosaktiutomo <bimasakticahyoutomo@gmail.com>
2025-09-24 21:18:06 +0800

    first push

---
+++ pertemuan_4/proyek/resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="flex flex-col items-center bg-slate-50 lg:w-[720px] w-[540px] px-4 py-2 shadow-md text-indigo-800 border border-indigo-100 rounded-b-xl font-medium">
+    <div class="flex justify-between w-full">
+        <div class="w-20 font-bold">
+            <h1>FrameWork</h1>
+        </div>
+        <h1 class="hover:text-indigo-700 duration-100"><a href="">Dashboard</a></h1>
+    </div>
+    <ul class="flex gap-2">
+        <li class="hover:translate-0.5 text-white duration-100 p-1.5 bg-gradient-to-br from-indigo-300 to-indigo-500 rounded-2xl"><a href="">Beranda</a></li>
+    </ul>
+</nav>
